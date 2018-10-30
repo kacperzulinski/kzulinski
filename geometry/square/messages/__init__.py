032192cb592cabb9da9a1bd01f55b947fe2c8169
kacperzulinski <kacper.zulinski@gmail.com>
2018-10-30 20:25:03 +0100

    update from pyCharm. test 01. some new information added.

---
+++ geometry/square/messages/__init__.py
@@ -0,0 +1,17 @@
+from one import message
+from second import message as message2
+from three import message as message3
+
+
+
+print message
+print message2
+print message3
+
+print 'Some new changes!!!!'
+
+print 'NewChanges????'
+
+
+
+
